609f680e3c2331356db38deac00b37c07f2a08ee
imperator6 <tino.hagemann@rwe.com>
2018-02-02 12:59:57 +0100

    Settings

--- server/src/main/groovy/tradingmaster/config/WebConfig.java
+++ server/src/main/groovy/tradingmaster/config/WebConfig.java
@@ -2,6 +2,7 @@ package tradingmaster.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
@@ -18,13 +19,23 @@ public class WebConfig extends WebMvcConfigurerAdapter{
         };
     }
 
+    /**
+     * redirect a user to the welcome page when he visits tha app without a
+     * destination url.
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/META-INF/resources/",
+            "classpath:/resources/", "classpath:/static/", "classpath:/public/" };
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
-        registry
-        .addResourceHandler("/**")
-        .addResourceLocations("/");
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
+
 }
--- server/src/main/groovy/tradingmaster/config/WebSecurityConfig.java
+++ server/src/main/groovy/tradingmaster/config/WebSecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -52,15 +51,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
     @Autowired
     JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
 
-        @Override
-        public void configure(WebSecurity web) throws Exception {
-            web
-                    .ignoring()
-                    .antMatchers("/index.html")
-                    .antMatchers("/resources/**");
-        }
-
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         String[] permited = new String[security.getIgnored().size()];
@@ -74,9 +64,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
                 .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/resources/**").permitAll()
                 .antMatchers("/api").permitAll()
                 .antMatchers("/socket").permitAll()
-                .antMatchers("/").permitAll()
                 //.antMatchers("/favicon.ico").permitAll()
                 .anyRequest().authenticated()
                 .and()
